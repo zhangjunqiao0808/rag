1d8c50ad10d737fd5584f0316a88d3f747b3a635
zjq0808 <zhangjunqiao666@gmail.com>
2026-08-26 15:25:20 +0800

    准备环境

---
+++ 01 配置大模型key.py
@@ -0,0 +1,30 @@
+from openai import OpenAI
+import os
+
+client = OpenAI(
+    # 如果没有配置环境变量，请用阿里云百炼API Key替换：api_key="sk-xxx"
+    #api_key="sk-ws-H.EYXIHLP.drWX.MEUCIQCssfTevF6Am6PLQ0yx5Uz_yk4G_Es-g_5V5YEEbWDmTwIgewykNMYhh5al1Top7nvyaBnyAcmzhEvGOYXz_BzyLc8",
+    base_url="https://llm-6c17orz0lf15g1ku.cn-beijing.maas.aliyuncs.com/compatible-mode/v1",
+)
+
+messages = [{"role": "user", "content": "你是谁"}]
+completion = client.chat.completions.create(
+    model="deepseek-v4-pro-0813",  # 您可以按需更换为其它深度思考模型
+    messages=messages,
+    extra_body={"enable_thinking": True},
+    stream=True
+)
+is_answering = False  # 是否进入回复阶段
+print("\n" + "=" * 20 + "思考过程" + "=" * 20)
+for chunk in completion:
+    if not chunk.choices:
+        continue
+    delta = chunk.choices[0].delta
+    if hasattr(delta, "reasoning_content") and delta.reasoning_content is not None:
+        if not is_answering:
+            print(delta.reasoning_content, end="", flush=True)
+    if hasattr(delta, "content") and delta.content:
+        if not is_answering:
+            print("\n" + "=" * 20 + "完整回复" + "=" * 20)
+            is_answering = True
+        print(delta.content, end="", flush=True)
